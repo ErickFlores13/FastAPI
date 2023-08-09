1e582529fa00ef986e11c25a37f13d7f7679be72
Erick Félix Flores <erickfelixflores13@gmail.com>
2023-08-08 23:33:25 -0700

    PUT upload

--- main.py
+++ main.py
@@ -21,5 +21,6 @@ def read_item(item_id: int, q: Union[str, None] = None):
     
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
-    return {"item_name": item.name,
+    return {"Status": f"El item con el id {item_id} fue actualizado",
+            "item_name": item.name,
             "item_id": item_id}
