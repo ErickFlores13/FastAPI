2dbbc60d92a55badc3cf2e4b5d99b42caa6165fd
Erick Félix Flores <erickfelixflores13@gmail.com>
2023-08-08 23:38:14 -0700

    Adding folder models

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 from typing import Union
 from fastapi import FastAPI
-from pydantic import BaseModel
+from models.item_model import Item
+
 
 #Creation of an a FastAPI application
 app = FastAPI()
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool,None] = None
 
 @app.get("/")
 def read_root():
